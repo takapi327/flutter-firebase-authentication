6774be91d3407f8bef0396f14ca71316ff86c57d
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2021-04-11 21:50:07 +0900

    Create TopPage file

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:provider/provider.dart';
 
 import 'package:flutter_firebase_authentication/mvc/state/auth_store.dart';
 import 'auth/auth_page.dart';
+import 'top_page.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -70,58 +71,6 @@ class AuthGuard extends StatelessWidget {
   }
 }
  */
-
-class TopPage extends StatelessWidget {
-
-  @override
-  Widget build(BuildContext context) {
-    return Consumer <AuthStore>(
-      builder: (context, authStore, _) {
-        return Scaffold(
-          appBar: AppBar(
-            title: Text('トップページ'),
-            actions: [
-              if (authStore.currentUser != null)
-              IconButton(
-                icon:      Icon(Icons.account_circle_sharp),
-                onPressed: () {
-                  Navigator.pushNamed(context, '/mypage');
-                }
-              ),
-
-              if (authStore.currentUser == null)
-                IconButton(
-                  icon:      Icon(Icons.login),
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/auth');
-                  }
-                ),
-            ],
-          ),
-          body: ClipRect(
-            child: Stack(
-              fit: StackFit.expand,
-              children: [
-                Container(
-                  padding: EdgeInsets.all(32),
-                  child: Column(
-                    children: [
-                      if (authStore.currentUser != null)
-                        Text(authStore.currentUser.toString()),
-                      if (authStore.currentUser == null)
-                        Text("未ログイン"),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-        );
-      },
-    );
-
-  }
-}
 
 class MyPage extends StatelessWidget {
 
