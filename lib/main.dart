54ca6224da52a5c3bb82575b1b4682fb8cd356de
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2021-04-11 21:51:13 +0900

    Create Mypage file

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import 'package:provider/provider.dart';
 import 'package:flutter_firebase_authentication/mvc/state/auth_store.dart';
 import 'auth/auth_page.dart';
 import 'top_page.dart';
+import 'mypage.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -71,48 +72,3 @@ class AuthGuard extends StatelessWidget {
   }
 }
  */
-
-class MyPage extends StatelessWidget {
-
-  @override
-  Widget build(BuildContext context) {
-    return Consumer <AuthStore>(
-      builder: (context, authStore, _) {
-        return Scaffold(
-          appBar: AppBar(
-            title: Text('マイページ'),
-          ),
-          body: ClipRect(
-            child: Stack(
-              fit: StackFit.expand,
-              children: [
-                Container(
-                  padding: EdgeInsets.all(32),
-                  child: Column(
-                    children: [
-                      Text(authStore.currentUser?.displayName?.toString() ?? "名前を登録していません"),
-                      Text(authStore.currentUser?.email.toString() ?? "メールアドレスを登録指定ません"),
-                      ElevatedButton(
-                        onPressed: () async {
-                          try {
-                            final FirebaseAuth auth = FirebaseAuth.instance;
-                            await auth.signOut();
-                            await authStore.setUser(null);
-                            Navigator.of(context).pop();
-                          } catch (e) {
-                            print(e.toString());
-                          }
-                        },
-                        child: Text("ログアウト"),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-        );
-      },
-    );
-  }
-}
