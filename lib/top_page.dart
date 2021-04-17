cb3cb9ac5605b41797d0d7c8b536ca01c9c463b8
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2021-04-17 15:10:42 +0900

    Fix Create PaymentMethod

--- lib/top_page.dart
+++ lib/top_page.dart
@@ -8,15 +8,7 @@ import 'package:flutter_firebase_authentication/mvc/state/auth_store.dart';
 
 class TopPage extends StatelessWidget {
 
-  String text        = 'Click the button to start the payment';
-  double totalCost   = 10.0;
-  double tip         = 1.0;
-  double tax         = 0.0;
-  double taxPercent  = 0.2;
-  int    amount      = 0;
-  bool   showSpinner = false;
-  String url         = 'https://us-central1-demostripe-b9557.cloudfunctions.net/StripePI';
-
+  String text = 'Click the button to start the payment';
 
   void checkIfNativePayReady(BuildContext context) async {
     print('started to check if native pay ready');
@@ -28,10 +20,6 @@ class TopPage extends StatelessWidget {
   }
 
   Future<void> createPaymentMethod(BuildContext context) async {
-    tax    = ((totalCost * taxPercent) * 100).ceil() / 100;
-    amount = ((totalCost + tip + tax) * 100).toInt();
-
-    print('amount in pence/cent which will be charged = $amount');
 
     //step 1: add card
     PaymentMethod paymentMethod = PaymentMethod();
@@ -66,34 +54,28 @@ class TopPage extends StatelessWidget {
 
     //step 1: add card
     PaymentMethod paymentMethod = PaymentMethod();
-    CardFormPaymentRequest options = CardFormPaymentRequest(
-      requiredBillingAddressFields: 'full',
-      prefilledInformation: PrefilledInformation(
-        billingAddress: BillingAddress(
-          name: 'Gunilla Haugeh',
-          line1: 'Canary Place',
-          line2: '3',
-          city: 'Macon',
-          state: 'Georgia',
-          country: 'US',
-          postalCode: '31217',
-        )
-      )
-    );
+
+    void setError(dynamic error) {
+      ErrorDialog(
+        title:      'Error',
+        content:    'It is not possible to pay with this card. Please try again with a different card',
+        buttonText: 'CLOSE'
+      );
+    }
 
     paymentMethod = await StripePayment.paymentRequestWithCardForm(
-      CardFormPaymentRequest()
-    );
+      CardFormPaymentRequest(),
+    ).catchError(setError);
 
     paymentMethod != null
-        ? print('Success:' + paymentMethod.id) //processPaymentAsDirectCharge(paymentMethod)
-        : showDialog(
-        context: context,
-        builder: (BuildContext context) => ErrorDialog(
-          title:      'Error',
-          content:    'It is not possible to pay with this card. Please try again with a different card',
-          buttonText: 'CLOSE'
-        )
+      ? print('Success:' + paymentMethod.id) //processPaymentAsDirectCharge(paymentMethod)
+      : showDialog(
+      context: context,
+      builder: (BuildContext context) => ErrorDialog(
+        title:      'Error',
+        content:    'It is not possible to pay with this card. Please try again with a different card',
+        buttonText: 'CLOSE'
+      )
     );
   }
 
@@ -101,9 +83,7 @@ class TopPage extends StatelessWidget {
   Widget build(BuildContext context) {
     StripePayment.setOptions(
       StripeOptions(
-        publishableKey: 'pk_test_51IZrtWJd0VKBQGZhjcHLHLyzjVnt850eXPu9GGgyVKgeC0D6QPY30KkaLWtgXcVstMHrUkXYsElC7UdCDzxeihbj00vnFMsF5c',
-        merchantId:     'Test',
-        androidPayMode: 'test'
+        publishableKey: 'pk_test_51IZrtWJd0VKBQGZhjcHLHLyzjVnt850eXPu9GGgyVKgeC0D6QPY30KkaLWtgXcVstMHrUkXYsElC7UdCDzxeihbj00vnFMsF5c'
       )
     );
     StripePayment.setStripeAccount(null);
@@ -143,48 +123,11 @@ class TopPage extends StatelessWidget {
                         Text(authStore.currentUser.toString()),
                       if (authStore.currentUser == null)
                         Text("未ログイン"),
-
-                      /*
-                      TextFormField(
-                        decoration: InputDecoration(
-                            border:    OutlineInputBorder(),
-                            labelText: "Card number",
-                            hintText:  "XXXX XXXX XXXX XXXX"
-                        ),
-                        style:  TextStyle(color: Colors.black),
-                        keyboardType: TextInputType.number,
-                        textInputAction: TextInputAction.next,
-                        onChanged: (String value) {
-                          authStore.changeEmail(value);
-                        },
-                      ),
-                      SizedBox(height: 16),
-                      TextFormField(
-                        decoration: InputDecoration(
-                          border:    OutlineInputBorder(),
-                          labelText: "Expiration Date",
-                          hintText:  "MM/YY"
-                        ),
-                        style:  TextStyle(color: Colors.black),
-                        keyboardType: TextInputType.number,
-                        textInputAction: TextInputAction.next,
-                        onChanged: (String text) {
-                          final arr = text.split('/');
-                          final month = int.tryParse(arr[0]);
-                          var year;
-                          if (arr.length == 2) {
-                            year = int.tryParse(arr[1]);
-                          }
-                        },
-                      ),
-                       */
                       SizedBox(height: 16),
                       ElevatedButton(
                         child: Text("カード登録", style: TextStyle(fontSize: 20)),
                         onPressed: () {
-                          //createPaymentMethod(context);
-                          createPaymentMethodNative(context);
-                          //checkIfNativePayReady(context);
+                          checkIfNativePayReady(context);
                         },
                       ),
                       SizedBox(height: 16),
