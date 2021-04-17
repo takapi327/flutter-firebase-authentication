61475577e873aefbe8a874dddb59e0c257b11a1b
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2021-04-17 18:37:28 +0900

    Create CreditCard UI

--- lib/stripe/payment_method.dart
+++ lib/stripe/payment_method.dart
@@ -3,6 +3,7 @@ import 'package:flutter_firebase_authentication/error_dialog.dart';
 import 'package:flutter_firebase_authentication/mvc/state/stripe_store.dart';
 import 'package:provider/provider.dart';
 
+import './credit_card.dart';
 import 'package:stripe_payment/stripe_payment.dart';
 
 class PaymentMethods extends StatelessWidget {
@@ -11,7 +12,7 @@ class PaymentMethods extends StatelessWidget {
     print('started to check if native pay ready');
     bool deviceSupportNativePay = await StripePayment.deviceSupportsNativePay();
     bool isNativeReady          = await StripePayment.canMakeNativePayPayments(
-        ['american_express', 'visa', 'maestro', 'master_card']
+      ['american_express', 'visa', 'maestro', 'master_card']
     );
     deviceSupportNativePay && isNativeReady ? createPaymentMethodNative(context) : createPaymentMethod(context);
   }
@@ -22,9 +23,9 @@ class PaymentMethods extends StatelessWidget {
     PaymentMethod paymentMethod = PaymentMethod();
 
     CreditCard _creditCard = CreditCard(
-        number:   '',
-        expMonth: 11,
-        cvc:      ''
+      number:   '',
+      expMonth: 11,
+      cvc:      ''
     );
 
     final PaymentMethodRequest _paymentMethodRequest = PaymentMethodRequest(
@@ -39,9 +40,9 @@ class PaymentMethods extends StatelessWidget {
         : showDialog(
         context: context,
         builder: (BuildContext context) => ErrorDialog(
-            title:      'Error',
-            content:    'It is not possible to pay with this card. Please try again with a different card',
-            buttonText: 'CLOSE'
+          title:      'Error',
+          content:    'It is not possible to pay with this card. Please try again with a different card',
+          buttonText: 'CLOSE'
         )
     );
   }
@@ -54,9 +55,9 @@ class PaymentMethods extends StatelessWidget {
 
     void setError(dynamic error) {
       ErrorDialog(
-          title:      'Error',
-          content:    'It is not possible to pay with this card. Please try again with a different card',
-          buttonText: 'CLOSE'
+        title:      'Error',
+        content:    'It is not possible to pay with this card. Please try again with a different card',
+        buttonText: 'CLOSE'
       );
     }
 
@@ -67,19 +68,11 @@ class PaymentMethods extends StatelessWidget {
     return paymentMethod.card;
   }
 
-  cardExp(StripeStore stripeStore) {
-    if (stripeStore.creditCard.expMonth.toString().length > 1) {
-      return stripeStore.creditCard.expMonth.toString() + '/' + stripeStore.creditCard.expYear.toString();
-    } else {
-      return '0' + stripeStore.creditCard.expMonth.toString() + '/' + stripeStore.creditCard.expYear.toString();
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     StripePayment.setOptions(
       StripeOptions(
-          publishableKey: 'pk_test_51IZrtWJd0VKBQGZhjcHLHLyzjVnt850eXPu9GGgyVKgeC0D6QPY30KkaLWtgXcVstMHrUkXYsElC7UdCDzxeihbj00vnFMsF5c'
+        publishableKey: 'pk_test_51IZrtWJd0VKBQGZhjcHLHLyzjVnt850eXPu9GGgyVKgeC0D6QPY30KkaLWtgXcVstMHrUkXYsElC7UdCDzxeihbj00vnFMsF5c'
       )
     );
 
@@ -92,96 +85,23 @@ class PaymentMethods extends StatelessWidget {
           },
           child: stripeStore.creditCard.last4 == null
             ? Container(
-                height:  50,
-                alignment: Alignment.center,
-                child:   Text("カード登録", style: TextStyle(fontSize: 16)),
+                height:     50,
+                alignment:  Alignment.center,
+                child:      Text("カード登録", style: TextStyle(fontSize: 16)),
                 decoration: BoxDecoration(
                   gradient: LinearGradient(
-                      begin: FractionalOffset.topLeft,
-                      end:   FractionalOffset.bottomRight,
-                      colors: const [
-                        Color(0xffffffff),
-                        Color(0xffe6e6e6),
-                      ]
+                    begin: FractionalOffset.topLeft,
+                    end:   FractionalOffset.bottomRight,
+                    colors: const [
+                      Color(0xffffffff),
+                      Color(0xffe6e6e6),
+                    ]
                   ),
                   border:       Border.all(color: Colors.black12),
                   borderRadius: BorderRadius.circular(10),
                 ),
               )
-            : Stack(
-                children: [
-                  Container(
-                    height:  150,
-                    padding: const EdgeInsets.all(16),
-                    decoration: BoxDecoration(
-                        gradient: LinearGradient(
-                            begin: FractionalOffset.topLeft,
-                            end:   FractionalOffset.bottomRight,
-                            colors: const [
-                              Color(0xffffffff),
-                              Color(0xffe6e6e6),
-                            ]
-                        ),
-                        borderRadius: BorderRadius.circular(10),
-                        boxShadow:    [
-                          BoxShadow(
-                            color:        Colors.black26,
-                            spreadRadius: 1.0,
-                            blurRadius:   10.0,
-                            offset:       Offset(10, 10),
-                          )
-                        ]
-                    ),
-                  ),
-                  Container(
-                    alignment: Alignment.topRight,
-                    height:    150,
-                    padding:   EdgeInsets.all(8),
-                    child: Text(
-                      stripeStore.creditCard.brand.toUpperCase(),
-                      style: TextStyle(
-                          fontSize:   24,
-                          fontStyle:  FontStyle.italic,
-                          fontWeight: FontWeight.bold,
-                          color:      Colors.blue
-                      ),
-                    ),
-                  ),
-                  Container(
-                      alignment: Alignment.centerLeft,
-                      padding:   EdgeInsets.only(left: 16),
-                      height:    150,
-                      child: Icon(
-                        Icons.credit_card,
-                        size: 40,
-                      )
-                  ),
-                  Container(
-                    alignment: Alignment.centerRight,
-                    padding:   EdgeInsets.only(right: 32),
-                    height:    150,
-                    child: Text(
-                      '**** **** **** ' + stripeStore.creditCard.last4.toString(),
-                      style: TextStyle(
-                          fontSize: 20,
-                          color:    Colors.black
-                      ),
-                    ),
-                  ),
-                  Container(
-                    alignment: Alignment.bottomRight,
-                    height:    150,
-                    padding:   EdgeInsets.all(8),
-                    child: Text(
-                      cardExp(stripeStore),
-                      style: TextStyle(
-                          fontSize: 16,
-                          color:    Colors.black
-                      ),
-                    ),
-                  ),
-                ],
-              )
+            : UICreditCard()
         );
       }
     );
