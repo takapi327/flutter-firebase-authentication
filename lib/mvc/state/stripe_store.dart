35b757fec1e22f5ef0faab69ad3a99f66f3e24ec
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2021-04-18 18:14:36 +0900

    Detele unused

--- lib/mvc/state/stripe_store.dart
+++ lib/mvc/state/stripe_store.dart
@@ -6,34 +6,6 @@ class StripeStore extends ChangeNotifier {
   CreditCard creditCard = CreditCard();
 
   List<CreditCard> cardList = [];
-  /*
-  List<CreditCard> cardList = [
-    CreditCard(
-      brand:    'Visa',
-      last4:    '1111',
-      number:   '4111111111111111',
-      expMonth: 11,
-      expYear:  25,
-      cvc:      '327'
-    ),
-    CreditCard(
-      brand:    'MasterCard',
-      last4:    '4444',
-      number:   '5555555555554444',
-      expMonth: 12,
-      expYear:  25,
-      cvc:      '327'
-    ),
-    CreditCard(
-      brand:    'JCB',
-      last4:    '0000',
-      number:   '3530111333300000',
-      expMonth: 11,
-      expYear:  25,
-      cvc:      '327'
-    ),
-  ];
-   */
 
   void setCard(CreditCard card) {
     creditCard = card;
