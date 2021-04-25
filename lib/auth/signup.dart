3a4c97f07d4002ae7ff3a03b2f4c777153a2c126
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2021-04-25 11:49:34 +0900

    Fix email textfield

--- lib/auth/signup.dart
+++ lib/auth/signup.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 
 import 'package:flutter_firebase_authentication/mvc/state/auth_store.dart';
 
@@ -11,7 +12,7 @@ class FirebaseAuthSignUp extends StatelessWidget {
     return Consumer <AuthStore>(
       builder: (context, authStore, _) {
         return Scaffold(
-          appBar: AppBar(
+          appBar: NeumorphicAppBar(
             title: Text('ユーザー登録'),
           ),
           body: Padding(
@@ -20,22 +21,44 @@ class FirebaseAuthSignUp extends StatelessWidget {
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  TextFormField(
-                    decoration: InputDecoration(labelText: "メールアドレス"),
-                    onChanged: (String value) {
-                      authStore.changeEmail(value);
-                    },
+                  Neumorphic(
+                    style: NeumorphicStyle(
+                      boxShape: NeumorphicBoxShape.stadium(),
+                    ),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        contentPadding: EdgeInsets.all(12.0),
+                        labelText: "メールアドレス",
+                        prefixIcon: Icon(
+                          Icons.email,
+                          color: Colors.black54,
+                        ),
+                        enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white),
+                        ),
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white),
+                        ),
+                      ),
+                      keyboardType: TextInputType.emailAddress,
+                      onChanged: (String value) {
+                        authStore.changeEmail(value);
+                      },
+                    ),
                   ),
-                  const SizedBox(height: 8),
-                  TextFormField(
-                    decoration: InputDecoration(labelText: "パスワード(6文字以上)"),
-                    obscureText: true,
-                    onChanged: (String value) {
-                      authStore.changePassword(value);
-                    },
+                  const SizedBox(height: 24),
+                  Neumorphic(
+                    child: TextFormField(
+                      decoration: InputDecoration(labelText: "パスワード(6文字以上)"),
+                      obscureText: true,
+                      onChanged: (String value) {
+                        authStore.changePassword(value);
+                      },
+                    ),
                   ),
-                  const SizedBox(height: 8),
-                  ElevatedButton(
+                  const SizedBox(height: 32),
+                  NeumorphicButton(
+                    child: Text("ユーザー登録"),
                     onPressed: () async {
                       try {
                         authStore.changeLoading(true);
@@ -49,7 +72,6 @@ class FirebaseAuthSignUp extends StatelessWidget {
                         authStore.changeLoading(false);
                       }
                     },
-                    child: Text("ユーザー登録"),
                   ),
                 ],
               ),
